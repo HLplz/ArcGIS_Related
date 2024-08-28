45e375f750b8ba324f76ca8db426e5802d01fec0
HaiyueLI <92358826+HLplz@users.noreply.github.com>
2024-08-27 21:09:56 -0500

    Update nc_county_conversion.py

--- nc_county_conversion.py
+++ nc_county_conversion.py
@@ -1,77 +1,59 @@
 import arcpy
+from arcpy import env
 from arcpy.sa import *
-import netCDF4
 import os
+import pandas as pd
 
-# Set the workspace and environment settings
+arcpy.env.addOutputsToMap = False 
+variables = ['sfsm_inst','gws_inst', 'rtzsm_inst'] # input your variables of concern 
 
-arcpy.env.workspace = #workspace folder path
-arcpy.env.overwriteOutput = True
+netcdf_file_path = # your .nc folder path
+shapefile = # your shapefile 
 
-shapefile = # .shp file that defines geographical division
+nc_list = [f for f in os.listdir(netcdf_file_path) if f.endswith('.nc4')]
 
-# extract all .nc file in data folder
-# to be revised based on file type
-nc_list = []
-folder_path = #folder path
-for file_name in os.listdir(folder_path):
-    #print(file_name)
-    if '.nc' in file_name and '.aux' not in file_name:
-        nc_list.append(file_name)
-print(nc_list[0:5])
-
-# variables of interest
-variables = # list of variables of interest 
-
-# Load the NetCDF File and Extract Variables
-netcdf_file_path = ## path to data folder 
 
 for file in nc_list:
-    print('processing: ' + file)
-
-    netcdf_file = netcdf_file_path + file
-
-
-    raster_layers = []
+    print(file) # track progress 
+    filename_without_extension = os.path.splitext(file)[0]
+    netcdf_full_path = os.path.join(netcdf_file_path, file)
 
     for var in variables:
+        last_nine = filename_without_extension[-12:-4]
         raster_layer = arcpy.md.MakeNetCDFRasterLayer(
-            netcdf_file,
+            netcdf_full_path,        
             var,                # Var name
             "lon",              # may be LON/Lon or others depending on your .nc file
             "lat",              # may be LAT/Lat or others depending on your .nc file
-            var,                # Output Raster Layer name
-            band_dimension="",
-            dimension_values="",
+            f'{last_nine}_{var}',                # Output Raster Layer name
+            band_dimension="", 
+            dimension_values="", 
             value_selection_method="BY_VALUE"
         )
-        raster_layers.append(raster_layer)
+        output_tiff = os.path.join(netcdf_file_path, f'{last_nine}_{var}.tif')
+        arcpy.management.CopyRaster(raster_layer[0], output_tiff, '', None, '-999', 'NONE', 'NONE', '', 'NONE', 'NONE', 'TIFF', 'NONE', 'CURRENT_SLICE', 'NO_TRANSPOSE')
 
-
-    output_folder = #output path 
-    # Create the output folder if it doesn't exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    for i, raster_layer in enumerate(raster_layers):
-        output_table = os.path.join(output_folder, f"Variable_{variables[i]}.dbf")
+        output_table = os.path.join(netcdf_file_path, f'{last_nine}_{var}.dbf')
 
         arcpy.sa.ZonalStatisticsAsTable(
-            in_zone_data=shapefile,
+            in_zone_data=shapefile, 
             zone_field="GEOID",  # Adjust this field if needed based on your shapefile
-            in_value_raster=raster_layer,
-            out_table=output_table,
-            statistics_type="ALL"
+            in_value_raster=output_tiff, 
+            out_table=output_table, 
+            statistics_type="MEAN"
         )
-
-    ## csv_file_name is subject to change 
-    for i, raster_layer in enumerate(raster_layers):
-        dbf_table = os.path.join(output_folder, f"Variable_{variables[i]}.dbf")
-        csv_file_name = netcdf_file[-13:-7] + f"_{variables[i]}.csv"
-        csv_file = os.path.join(output_folder, csv_file_name)
-
-        if arcpy.Exists(dbf_table):
-            arcpy.TableToTable_conversion(dbf_table, output_folder, csv_file_name)
-        else:
-            print("dff doesn't exist or dbf error")
-
+        output_folder = # your output folder 
+        output_excel = os.path.join(output_folder, f'{last_nine}_{var}.xls')
+        arcpy.TableToExcel_conversion(output_table, output_excel)
+        
+        output_csv = os.path.join(output_folder, f'{last_nine}_{var}.csv')
+        df = pd.read_excel(output_excel)
+        df.to_csv(output_csv, index=False)
+        # Cleanup if wrriten into env
+        arcpy.Delete_management(output_tiff)
+        arcpy.Delete_management(output_table)
+        arcpy.Delete_management(output_excel)
+        #arcpy.Delete_management(output_csv)
+        #arcpy.Delete_management(csv_file_name)
+        arcpy.Delete_management(raster_layer)
+        arcpy.Delete_management('in_memory')
